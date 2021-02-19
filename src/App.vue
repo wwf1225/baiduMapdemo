01613e1a1a2a13f479d1b002daf06e9f1e9438e4
王伟峰 <wangweifeng@sword-tech.com>
2021-02-19 11:22:08 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="baidumap" id="allmap"></div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted() {
+    this.baiduMap();
+  },
+  methods: {
+    baiduMap() {
+      var map = new window.BMap.Map("allmap"); //创建地图
+
+      map.enableScrollWheelZoom();
+      // 启用滚轮放大缩小;
+      var point = new window.BMap.Point(114.12883, 22.688615); //设置地图位置
+      map.centerAndZoom(point, 12);
+      var marker = new window.BMap.Marker(point); // 创建标注
+      map.addOverlay(marker); // 将标注添加到地图中
+    },
+  },
+};
 </script>
 
 <style>
@@ -24,5 +40,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.baidumap {
+  width: 500px;
+  height: 200px;
+  border: 1px solid red;
 }
 </style>
